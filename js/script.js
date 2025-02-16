52bd63faac4da37d8e43f1bec43d439651e2648a
Axel Seger <axelseger@MacBook-Pro-som-tillhor-Axel.local>
2025-02-16 19:08:16 +0100

    Added Dark Mode

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ const year = d.getFullYear('Y');
 const copyright = document.querySelector('#copyright');
 
 copyright.innerHTML = `&#169; ${year} ${title}`;
+
+// Toogle dark mode
+
+logo.onclick = () => {
+    let body = document.querySelector('body');
+    let currentBg = getComputedStyle(body).backgroundColor;
+
+    if (currentBg === "rgb(255, 255, 255)") { // White in RGB
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+};
+
+/*
+logo.onclick = () => {
+    if (document.querySelector('body').style.backgroundColor === "white") {
+        document.querySelector('body').style.backgroundColor = "black";
+        document.querySelector('body').style.color = "white";
+    } else {
+        document.querySelector('body').style.backgroundColor = "white";
+        document.querySelector('body').style.color = "black";
+    }
+}*/
